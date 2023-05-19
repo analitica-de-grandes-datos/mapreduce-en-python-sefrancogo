76e28e4ae285757efe935f73a9487654a4fdf2c7
sefrancogo <64169379+sefrancogo@users.noreply.github.com>
2023-05-19 14:29:39 -0500

    Update mapper.py

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+        for line in sys.stdin:
+                words=  line.split(',')
+                if( len(words) >2):
+                        sys.stdout.write("{}\t1\n".format(words[2])) 
