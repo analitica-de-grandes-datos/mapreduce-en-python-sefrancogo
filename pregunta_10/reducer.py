69c1a9f3ce64bd79ea213083a58605f91dc2c24d
sefrancogo <64169379+sefrancogo@users.noreply.github.com>
2023-05-19 15:05:09 -0500

    Update reducer.py

--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,17 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys 
+
+if __name__ ==  '__main__':
+
+        curkey = None
+        for line in sys.stdin:
+                key, val = line.strip().split("\t")
+                val = int(val)
+                if curkey == key: sys.stdout.write(",{}".format(val)) 
+                else:
+                        if curkey is not None: sys.stdout.write("\n") 
+                        sys.stdout.write("{}\t{}".format(key,val))    
+                        curkey = key
+        sys.stdout.write("\n")
