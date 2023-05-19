e272c1385a616db4af0964dca01be24408ebc2c4
sefrancogo <64169379+sefrancogo@users.noreply.github.com>
2023-05-19 14:51:58 -0500

    Update mapper.py

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+        for s in sys.stdin:
+                let, date, val = s.strip().split()
+                sys.stdout.write("{}\t1\n".format(let))
