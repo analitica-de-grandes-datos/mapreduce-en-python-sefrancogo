cb3645623cf185f1cdd4e0b0a5ae762009c96464
sefrancogo <64169379+sefrancogo@users.noreply.github.com>
2023-05-19 14:59:38 -0500

    Update mapper.py

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -7,10 +7,8 @@ if __name__  == '__main__':
 
         for line in sys.stdin:
                 letter, date, num = line.strip().split()
-
                 if len(num) ==1:
                         num= "00" + num
                 if len(num) == 2:
                         num = "0" + num
-
                 sys.stdout.write("{},{},{}\n".format(letter, num, date))
