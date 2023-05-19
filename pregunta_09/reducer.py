6f197cc1542a9a4b94908fc1c21857dfc4ca41b5
sefrancogo <64169379+sefrancogo@users.noreply.github.com>
2023-05-19 15:03:15 -0500

    Update reducer.py

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+        cont =0
+        while cont < 6:
+                cont +=1
+                num, letter, date = sys.stdin.readline().strip().split(",")
+
+                num= int(num)
+
+                sys.stdout.write("{}   {}   {}\n".format(letter, date, num))
