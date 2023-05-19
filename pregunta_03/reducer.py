174cf57bf226c58eca964f63b65a8409e660a7c6
sefrancogo <64169379+sefrancogo@users.noreply.github.com>
2023-05-19 14:39:40 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+        for line in sys.stdin:
+                val, key = line.strip().split(",")
+                sys.stdout.write("{},{}\n".format(key, val))
